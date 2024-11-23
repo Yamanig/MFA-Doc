08c144eb7d73f3c8390ae2daa28023933e0e17fa
Yamanig <Yamanig@gmail.com>
2024-11-23 08:04:23 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MFA-Doc
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Yamanig/MFA-Doc)
---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  swcMinify: false,
+  webpack: (config) => {
+    config.optimization.minimize = false;
+    return config;
+  }
+};
+
+module.exports = nextConfig;
